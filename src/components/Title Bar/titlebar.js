ba75b919db56f564c06b1eed18923e2880ff663d
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-02-14 11:58:53 +0800

    Add Menu Header

--- src/components/Title Bar/titlebar.js
+++ src/components/Title Bar/titlebar.js
@@ -1,19 +1,48 @@
+import { useState } from 'react';
 import './titlebar.css';
+import './headerMenu.css';
 
-function openMenu(x, y) {
-    //ipcRenderer.send(`display-app-menu`, { x, y });
+function openMenu(x, y, menuOpen, setMenuOpen) {
+    function checkoutside(event) {
+        if (document.querySelector(".headerMenu").contains(event.target)) {
+            console.log('clicked inside');
+        } else {
+            console.log('clicked outside');
+            setMenuOpen(false)
+            //document.body.removeEventListener('click', checkoutside)
+        }
+    }
+    if (menuOpen) {
+        setMenuOpen(false)
+        //document.body.removeEventListener('click', checkoutside)
+    } else {
+        setMenuOpen(true)
+        //setInterval(function () {
+            //document.body.addEventListener('click', checkoutside)
+        //}, 10)
+    }
+}
+
+function headerMenu() {
+    return (
+        <div className="headerMenu">
+        </div>
+    )
 }
 
 export default function TitleBar(props) {
+    const [menuOpen, setMenuOpen] = useState(false)
     return (<div className="titleBar" style={{ width: props.winsize.width, height: props.titleMenuBarSpace }}>
         <div>
-            <div className="titleBar-onemenu" onClick={() => openMenu(0, 5)}>
+            <div className="titleBar-onemenu" onClick={() => openMenu(0, 5, menuOpen, setMenuOpen)}>
                 ☰
             </div>
+
             <div className="titleBar-draggable" />
             <div className="titleBar-buttons">
-                <span className="titleBar-button" id="titleBar-feedback" onClick={() => {}/*ipcRenderer.send("window-minimize")*/}>Feedback</span>
+                <span className="titleBar-button" id="titleBar-feedback" onClick={() => { }/*ipcRenderer.send("window-minimize")*/}>Feedback</span>
             </div>
+            {menuOpen ? headerMenu() : null}
         </div>
     </div>)
 }
